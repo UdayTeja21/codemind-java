8c69f51bbd856e920be5d9b1372998a732ad19d2
UdayTeja21 <97437700+UdayTeja21@users.noreply.github.com>
2023-02-01 19:58:02 +0530

    import java.util.*; class average {     public static void main(String args[])     {
            float a,b,c;
            Scanner sc=new Scanner(System.in);
            a=sc.nextInt();
            b=sc.nextInt();
            c=((a+b)/2);
            System.out.format("%.4f",c);
        }
    }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class average
+{
+    public static void main(String args[])
+    {
+        
+        float a,b,c;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=((a+b)/2);
+        System.out.format("%.4f",c);
+    }
+}
