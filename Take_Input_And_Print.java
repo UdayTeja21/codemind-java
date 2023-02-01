de0d6bc687399fbc0a137b1c2e53f351b9c88721
UdayTeja21 <97437700+UdayTeja21@users.noreply.github.com>
2023-02-01 20:23:52 +0530

    import java.util.Scanner; class print {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String A;         A=sc.nextLine();         System.out.println(A);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class print
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String A;
+        A=sc.nextLine();
+        System.out.println(A);
+    }
+}
