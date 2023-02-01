a7cd0b6124085f49e86df11f7e0e20f8552e142d
UdayTeja21 <97437700+UdayTeja21@users.noreply.github.com>
2023-02-01 22:42:19 +0530

    import java.util.*; class circle {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a;         float b;         double p=3.14;         a=sc.nextInt();         b=(float)(p*a*a);         System.out.format("%.2f",b);     } }

--- Area_of_a_circle.java
+++ Area_of_a_circle.java
@@ -3,12 +3,12 @@ class circle
 {
     public static void main(String args[])
     {
-        Scanner sc=new Scanner (System.in);
+        Scanner sc=new Scanner(System.in);
         int a;
         float b;
         double p=3.14;
         a=sc.nextInt();
         b=(float)(p*a*a);
-       System.out.format("%.2f",b);
+        System.out.format("%.2f",b);
     }
 }
